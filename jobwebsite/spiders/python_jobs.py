84cfb2b67879dada84f1f1c43ed851e6446494f0
Nazar Psheiuk <nazar.psheiuk@gmail.com>
2023-10-20 19:28:20 +0300

    scrapyng files

--- jobwebsite/spiders/python_jobs.py
+++ jobwebsite/spiders/python_jobs.py
@@ -1,5 +1,11 @@
+from collections import Counter
+
 import scrapy
 from scrapy.http import Response
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import nltk
+from bad_words import BadWords
 
 
 class PythonJobsSpider(scrapy.Spider):
@@ -10,16 +16,47 @@ class PythonJobsSpider(scrapy.Spider):
     def parse(self, response: Response, **kwargs):
         for jobs in response.css(".job-list-item"):
             yield {
-                "jobs_name": jobs.css(".job-list-item__link::text").get().strip(),
+                "jobs_name": (jobs.css(".job-list-item__link::text").get().strip()),
                 "company_name": jobs.css(".mr-2::text").getall()[2].strip(),
-                "date_time": jobs.css(".text-muted").css(".mr-2::attr(title)").get(),
-                "description": jobs.css(".job-list-item__description span::attr(data-original-text)").get(),
-                "localization": jobs.css(".job-list-item__job-info > span::text").getall()[-3],
-                "experience": jobs.css(".job-list-item__job-info span.nobr::text").getall()[-2],
-                "english_level": jobs.css(".job-list-item__job-info span.nobr::text").getall()[-1]
+                "date_time": (jobs.css(".text-muted").css(".mr-2::attr(title)").get()),
+                "description": (
+                    jobs.css(
+                        ".job-list-item__description span::attr(data-original-text)"
+                    ).get()
+                ),
+                "localization": jobs.css(
+                    ".job-list-item__job-info > span::text"
+                ).getall()[-3],
+                "experience": jobs.css(
+                    ".job-list-item__job-info span.nobr::text"
+                ).getall()[-2],
+                "english_level": jobs.css(
+                    ".job-list-item__job-info span.nobr::text"
+                ).getall()[-1],
+                "words": self._select_words(response=jobs),
             }
 
-        next_page = response.css(".pagination").css(".page-item a::attr(href)").getall()[-1]
+        next_page = (
+            response.css(".pagination").css(".page-item a::attr(href)").getall()[-1]
+        )
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
+
+    def _select_words(sels, response: Response):
+        nltk.download("punkt")
+        nltk.download("stopwords")
+        text = (
+            response.css(
+                ".job-list-item__description span::attr(data-original-text)"
+            ).get(),
+        )
+
+        stop_words = set(stopwords.words("english") + BadWords.custom_stopwords)
+        if text[0] is not None:
+            tokens = word_tokenize(text[0])
+            filtered_tokens = [
+                word for word in tokens if word.lower() not in stop_words
+            ]
+            return dict(Counter(filtered_tokens))
+        return text
